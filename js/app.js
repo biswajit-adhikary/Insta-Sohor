a304ad2b4f941310b73e476a9e2cd3675383d169
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-03-11 03:14:19 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -33,18 +33,21 @@ const displayContent = (text) => {
 const switchTab = (id) => {
   if (id === "posts") {
     document.getElementById("posts").style.display = "grid";
+    document.getElementById("questions").style.display = "block";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("questions").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById("questions").style.display = "none";
 
     displayReportedPosts();
   }
