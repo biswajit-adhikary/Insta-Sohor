52295873ebb7f8933dd500320822f331425e4701
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-03-11 00:00:55 +0600

    profile picture issue fixed

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
+  const userImage = post.userImage;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -62,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -95,8 +96,7 @@ const createPost = (post) => {
 
                   <div class="post__indicators"></div>
 
-                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id
-    })">
+                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id})">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
